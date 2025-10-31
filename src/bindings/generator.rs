95d03890d2c79a37ba6ab0da6dce088e77a99d34
Jacob Gelman <3182119+ladvoc@users.noreply.github.com>
2025-10-31 16:47:07 +1100

    Rename

--- src/bindings/generator.rs
+++ src/bindings/generator.rs
@@ -1,27 +1,29 @@
-use std::borrow::Borrow;
-use uniffi_bindgen::{ComponentInterface, interface::{AsType, FfiDefinition, FfiType}};
 use anyhow::{Context, Result};
 use askama::Template;
+use std::borrow::Borrow;
+use uniffi_bindgen::{
+    ComponentInterface,
+    interface::{AsType, FfiDefinition, FfiType},
+};
 
 use crate::bindings::filters;
 
 pub struct Bindings {
-    pub napi_interop_rust_file_contents: String,
+    pub node_interop_rust_file_contents: String,
     pub node_ts_file_contents: String,
 }
 
 #[derive(Template)]
-#[template(escape = "none", path = "napi_interop.rs")]
-struct NapiInteropRs<'ci> {
+#[template(escape = "none", path = "node_interop.rs")]
+struct NodeInteropRs<'ci> {
     ci: &'ci ComponentInterface,
 }
 
-impl<'ci> NapiInteropRs<'ci> {
+impl<'ci> NodeInteropRs<'ci> {
     pub fn new(ci: &'ci ComponentInterface) -> Self {
         Self { ci }
     }
 }
-
 
 #[derive(Template)]
 #[template(escape = "none", path = "node.ts")]
@@ -36,11 +38,15 @@ impl<'ci> NodeTs<'ci> {
 }
 
 pub fn generate_node_bindings(ci: &ComponentInterface) -> Result<Bindings> {
-    let napi_interop_rust_file_contents = NapiInteropRs::new(ci).render().context("failed to render napi interop rs file")?;
-    let node_ts_file_contents = NodeTs::new(ci).render().context("failed to render node ts file")?;
+    let node_interop_rust_file_contents = NodeInteropRs::new(ci)
+        .render()
+        .context("failed to render node-ffi-rs interop rs file")?;
+    let node_ts_file_contents = NodeTs::new(ci)
+        .render()
+        .context("failed to render node ts file")?;
 
     Ok(Bindings {
-        napi_interop_rust_file_contents,
+        node_interop_rust_file_contents,
         node_ts_file_contents,
     })
 }
