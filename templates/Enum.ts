4eb264127a2d182b7f0ebc569950b37188283b76
Jacob Gelman <3182119+ladvoc@users.noreply.github.com>
2025-11-19 17:07:36 +1100

    Enums with associated values

--- templates/Enum.ts
+++ templates/Enum.ts
@@ -22,7 +22,7 @@ export type {{ enum_def.name() | typescript_class_name }} =
     | "{{ variant_name }}"
     {%- else -%}
     | {
-        type: "{{ variant_name }}",
+        tag: "{{ variant_name }}",
         {%- if !variant.fields().is_empty() %}
         inner: {% call variant_inner_fields(variant) %}
         {%- endif %}
@@ -44,8 +44,29 @@ export const {{ enum_def.name() | typescript_ffi_converter_struct_enum_object_na
                 default: throw new UniffiInternalError.UnexpectedEnumCase();
             }
             {%- else -%}
-            // TODO: Handle associated values
-            throw new UniffiInternalError.Unimplemented();
+            switch (ordinalConverter.read(from)) {
+                {%- for variant in enum_def.variants() %}
+                {%- let variant_name = variant.name() | typescript_var_name %}
+                case {{ loop.index0 + 1 }}:
+                    return {
+                        tag: "{{ variant_name }}",
+                        inner: {% if !variant.has_nameless_fields() -%} {
+                            {%- for field in variant.fields() %}
+                            {{ field.name() | typescript_var_name }}: {{ field.as_type().borrow() | typescript_ffi_converter_name }}.read(from)
+                            {%- if !loop.last %}, {% endif -%}
+                            {%- endfor %}
+                        }
+                        {%- else -%} [
+                            {%- for field in variant.fields() %}
+                            {{ field.as_type().borrow() | typescript_ffi_converter_name }}.read(from)
+                            {%- if !loop.last %}, {% endif -%}
+                            {%- endfor %}
+                        ]
+                        {%- endif %}
+                    };
+                {%- endfor %}
+                default: throw new UniffiInternalError.UnexpectedEnumCase();
+            }
             {%- endif %}
         }
         write(value: {{ type_name }}, into: RustBuffer): void {
@@ -58,16 +79,47 @@ export const {{ enum_def.name() | typescript_ffi_converter_struct_enum_object_na
                 default: throw new UniffiInternalError.UnexpectedEnumCase();
             }
             {%- else -%}
-            // TODO: Handle associated values
-            throw new UniffiInternalError.Unimplemented();
+            switch (value.tag) {
+                {%- for variant in enum_def.variants() %}
+                {%- let variant_name = variant.name() | typescript_var_name %}
+                case "{{ variant_name }}":
+                    ordinalConverter.write({{ loop.index0 + 1 }}, into);
+                    {%- for field in variant.fields() %}
+                    {% let converter_name = field.as_type().borrow() | typescript_ffi_converter_name %}
+                    {%- if !variant.has_nameless_fields() -%}
+                    {{ converter_name }}.write(value.inner.{{ field.name() | typescript_var_name }}, into);
+                    {%- else -%}
+                    {{ converter_name }}.write(value.inner[{{ loop.index0 }}], into);
+                    {%- endif -%}
+                    {%- endfor %}
+                    break;
+                {%- endfor %}
+                default: throw new UniffiInternalError.UnexpectedEnumCase();
+            }
             {%- endif %}
         }
         allocationSize(value: {{ type_name }}): number {
             {% if enum_def.is_flat() -%}
             return ordinalConverter.allocationSize(0);
             {%- else -%}
-            // TODO: Handle associated values
-            throw new UniffiInternalError.Unimplemented();
+            switch (value.tag) {
+                {%- for variant in enum_def.variants() %}
+                {%- let variant_name = variant.name() | typescript_var_name %}
+                case "{{ variant_name }}":
+                    return ordinalConverter.allocationSize({{ loop.index }})
+                    {%- for field in variant.fields() -%}
+                        {% let converter_name = field.as_type().borrow() | typescript_ffi_converter_name %}
+                        + {{ converter_name }}.allocationSize(
+                            {%- if !variant.has_nameless_fields() -%}
+                            value.inner.{{ field.name() | typescript_var_name }}
+                            {%- else -%}
+                            value.inner[{{ loop.index0 }}]
+                            {%- endif -%}
+                        )
+                    {%- endfor -%};
+                {%- endfor %}
+                default: throw new UniffiInternalError.UnexpectedEnumCase();
+            }
             {%- endif %}
         }
     }
