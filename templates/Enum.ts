22201f2180a586d085333fd965bf2ad70ec6800b
Jacob Gelman <3182119+ladvoc@users.noreply.github.com>
2025-11-19 15:44:14 +1100

    Break

--- templates/Enum.ts
+++ templates/Enum.ts
@@ -32,11 +32,11 @@ export type {{ enum_def.name() | typescript_class_name }} =
 
 
 export const {{ enum_def.name() | typescript_ffi_converter_struct_enum_object_name }} = (() => {
-    {%- let converter = "FfiConverterInt32" %}
+    const ordinalConverter = FfiConverterInt32;
     {%- let type_name = enum_def.name() | typescript_class_name %}
     class FFIConverter extends AbstractFfiConverterByteArray<{{ type_name }}> {
         read(from: RustBuffer): {{ type_name }} {
-            switch ({{ converter }}.read(from)) {
+            switch (ordinalConverter.read(from)) {
                 {%- for variant in enum_def.variants() %}
                 {%- let variant_name = variant.name() | typescript_var_name %}
                 case {{ loop.index0 + 1 }}: return "{{ variant_name }}";
@@ -48,13 +48,13 @@ export const {{ enum_def.name() | typescript_ffi_converter_struct_enum_object_na
             switch (value) {
                 {%- for variant in enum_def.variants() %}
                 {%- let variant_name = variant.name() | typescript_var_name %}
-                case "{{ variant_name }}": {{ converter }}.write({{ loop.index0 + 1 }}, into);
+                case "{{ variant_name }}": ordinalConverter.write({{ loop.index0 + 1 }}, into); break;
                 {%- endfor %}
                 default: throw new UniffiInternalError.UnexpectedEnumCase();
             }
         }
         allocationSize(value: {{ type_name }}): number {
-            return {{ converter }}.allocationSize(0);
+            return ordinalConverter.allocationSize(0);
         }
     }
     return new FFIConverter();
