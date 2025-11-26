472c7b2cb47478956394ad8ddd561926390fb2bc
Ryan Gaus <rsg1egoman@gmail.com>
2025-11-26 16:35:20 -0500

    docs: add missing period

--- src/lib.rs
+++ src/lib.rs
@@ -71,7 +71,7 @@ pub struct Args {
     /// Changes the extension used in `import`s within the final generated output. This exists
     /// because depending on packaging / tsc configuration, the import path extensions may be
     /// expected to end in different extensions. For example, tsc often requires .js extensions
-    /// on .ts files it imports, etc
+    /// on .ts files it imports, etc.
     #[arg(long, action, value_enum, default_value_t=OutputImportExtension::default())]
     out_import_extension: OutputImportExtension,
 
